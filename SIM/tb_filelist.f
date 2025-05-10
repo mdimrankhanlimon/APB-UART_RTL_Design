93a2e976c4bdf1fb589d0cb09011b04859d81942
Md. Imran Khan <111862722+mdimrankhanlimon@users.noreply.github.com>
2025-05-10 13:07:17 +0600

    Add files via upload

---
+++ SIM/tb_filelist.f
@@ -0,0 +1,16 @@
+//../TB/TxTB/BaudTest.v
+//../TB/TxTB/PisoTest.v
+//../TB/TxTB/ParityTest.v
+//../TB/TxTB/FifoTest.v
+//../TB/TxTB/TxTest.v
+
+//../TB/RxTB/BaudTest.v
+//../TB/RxTB/CheckTest.v
+//../TB/RxTB/DeFrameTest.v
+//../TB/RxTB/SIPOTest.v
+//../TB/RxTB/RxTest.v
+
+
+//../TB/UartTB/tb_Duplex.v
+//../TB/UartTB/Uart_core_tb.v
+../TB/UartAPB_TB/uart_apb_tb.v
